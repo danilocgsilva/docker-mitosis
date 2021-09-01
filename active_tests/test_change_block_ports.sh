0047f3eae24204603a073c1d577a2263bc448703
Danilo Silva <contact@danilocgsilva.me>
2021-08-31 21:29:35 -0300

    Larger chunks are failing
    To detect ports span. Check tests.

--- active_tests/test_change_block_ports.sh
+++ active_tests/test_change_block_ports.sh
@@ -77,12 +77,21 @@ test_span_lines_for_ports_4() {
     this_assert $EXPECTED_RESULTS $RESULTS "$1"
 }
 
-test_find_ports_starting_line_2 $DOTFILE
-test_find_ports_starting_line_1 $DOTFILE
-test_find_ports_starting_line_0 $DOTFILE
-test_find_ports_starting_line_6 $DOTFILE
-# test_span_lines_for_ports_1 $DOTFILE
-# test_span_lines_for_ports_2 $DOTFILE
-# test_span_lines_for_ports_3 $DOTFILE
-# test_span_lines_for_ports_4 $DOTFILE
-# test_span_lines_for_ports_5 $DOTFILE
+test_span_lines_for_ports_5() {
+    echo '* test_span_lines_for_ports_5'
+
+    RESULTS=$(span_lines_for_ports mocks/mock_larger_span_2.txt)
+    EXPECTED_RESULTS=2
+
+    this_assert $EXPECTED_RESULTS $RESULTS "$1"
+}
+
+# test_find_ports_starting_line_2 $DOTFILE
+# test_find_ports_starting_line_1 $DOTFILE
+# test_find_ports_starting_line_0 $DOTFILE
+# test_find_ports_starting_line_6 $DOTFILE
+test_span_lines_for_ports_1 $DOTFILE
+test_span_lines_for_ports_2 $DOTFILE
+test_span_lines_for_ports_3 $DOTFILE
+test_span_lines_for_ports_4 $DOTFILE
+test_span_lines_for_ports_5 $DOTFILE
